39a35bdddf3fa8400e892306c3293385065a3ffa
Tamiris Simoneli <tamiris.simoneli@gmail.com>
2018-01-28 09:28:09 -0200

    change news sources by select

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const apiKey = '8e9fd4f18cb346a08113df08733d912c';
 const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
 
-window.addEventListener('load', e => {
+window.addEventListener('load', async e => {
   updateNews();
+  await updateSources();
+  sourceSelector.value = defaultSource;
+
+  sourceSelector.addEventListener('change', e => {
+    updateNews(e.target.value);
+  })
 });
 
-async function updateNews() {
-  const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`)
+async function updateSources() {
+  const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+  const json = await res.json();
+
+  sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+  const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`)
   const json = await res.json();
 
   main.innerHTML = json.articles.map(createArticle).join('\n');
